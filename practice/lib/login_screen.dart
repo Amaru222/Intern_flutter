00dace3d78acfd0f249849f8f16acc50ce8588e1
Đức Lê <amaru@MacBook-cua-uc.local>
2024-06-11 11:43:45 +0700

    update report and create loginscreen for pratice

---
+++ practice/lib/login_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/background.png'),
+              fit: BoxFit.cover)),
+      child: Center(
+        child: Text('Welcome to', style: TextStyle(fontSize: 30)),
+      ),
+      
+    ));
+  }
+}

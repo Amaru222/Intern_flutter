91df2905c346b7043541b04fad7c782d264822c6
Đức Lê <amaru@MacBook-cua-uc.local>
2024-06-11 20:36:51 +0700

    login screen

--- practice/lib/login_screen.dart
+++ practice/lib/login_screen.dart
@@ -11,15 +11,78 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/images/background.png'),
-              fit: BoxFit.cover)),
-      child: Center(
-        child: Text('Welcome to', style: TextStyle(fontSize: 30)),
+        body: Center(
+      child: Container(
+        width: MediaQuery.of(context).size.width + 0.8,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(
+                    'https://images.unsplash.com/photo-1614368558214-f8922bd49987?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+                opacity: 0.7,
+                fit: BoxFit.cover)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Welcome to', style: TextStyle(fontSize: 40)),
+            Image.network('https://i.imgur.com/Shu2fPn.png'),
+            SizedBox(height: 35),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              // height: MediaQuery.of(context).size.height * 0.8,
+              child: TextFormField(
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    hintText: 'Email'),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              // height: MediaQuery.of(context).size.height * 0.8,
+              child: TextFormField(
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    hintText: 'Password'),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: ElevatedButton(
+                  onPressed: () {
+                    // Handle button press
+                  },
+                  child: Text('Submit'),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xffed9937),
+                      padding: EdgeInsets.symmetric(
+                          vertical: 20), // Adjust the height of the button
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ))),
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Icon(Icons.apple),
+              SizedBox(width: 20),
+              Icon(Icons.apple),
+              SizedBox(width: 20),
+              Icon(Icons.facebook),
+              SizedBox(width: 20),
+            ]),
+          ],
+        ),
       ),
-      
     ));
   }
 }
